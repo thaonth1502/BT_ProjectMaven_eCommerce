3dc804d15199a1c04ae80847858446756d49a75d
thaonth1502 <huongthao89@gmail.com>
2024-08-11 17:54:49 +0700

    add bai 16

--- src/test/java/common/BaseTest.java
+++ src/test/java/common/BaseTest.java
@@ -14,7 +14,6 @@ public class BaseTest {
    public void createDriver(){
        driver = new ChromeDriver();
        driver.manage().window().maximize();
-       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
    }
 
